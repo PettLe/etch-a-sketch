27b5e107db91281377c29d22e68e5f29b323f259
PettLe <petewaterz@gmail.com>
2021-09-20 14:18:58 +0300

    finally made the button work

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-/*Maybe first create few helpful global variables. Can make them local later*/
+//FEW USEFUL GLOBAL VARIABLES
 const grid = document.querySelector("#grid-container");
 const box = document.createElement("div");
 let size = 16;
 const button = document.querySelector(".clear");
 
-/* Creating the canvas grid by iterating. Then adding eventListener for the
+/* CANVAS GRID
+Creating the canvas grid by iterating. Then adding eventListener for the
 mouse movement.*/
 function canvas() {
 for (i = 0; i < size; i++) {
@@ -22,16 +23,16 @@ for (i = 0; i < size; i++) {
 }
 }
 }
-/*Need a CLEAR button. Probably similiar to the one I used in the RPS-UI.
-Also when clicked, prompt should pop up asking for the size of the new canvas.
-That number should link to the first phase.
-Sliders probably acceptable and nifty solution too.*/
 
+/*THE CLEAR BUTTON*/
 button.addEventListener("click", function() {
     let newSize = parseInt(prompt("New size?"));
     if (newSize > 1 && newSize < 101) {
         size = newSize;
-        canvas();
+        while (grid.firstChild) {
+            grid.removeChild(grid.lastChild);
+        }
+       canvas();
     } else {
         alert("Please use a number between 2 and 100");
     }
